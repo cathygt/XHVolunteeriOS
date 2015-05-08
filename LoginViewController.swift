0548965b1d0b58ca2ab2f8e87032d865f2580deb
cathygt <847463414@qq.com>
2015-05-08 17:54:17 +0800

    WT 5-8

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -18,6 +18,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var LoginButton: UIButton!
     
+    
     let UserNameKey = ""
     let PwdKey = ""
     let IsFirstLaunch = "ifl"
